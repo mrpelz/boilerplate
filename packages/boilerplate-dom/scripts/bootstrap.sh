195edeb4f78561e418c884340833c8db27c9644f
Lennart Pelz <mail@mrpelz.de>
2024-01-17 11:26:33 +0100

    feat: boilerplate-dom

---
+++ packages/boilerplate-dom/scripts/bootstrap.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+
+## ENVIRONMENT
+
+BOILERPLATE_MODULE_NAME="@mrpelz/boilerplate-common"
+BOILERPLATE_MODULE_PATH="$(realpath --relative-to=. "$(npm ls --parseable --silent "$BOILERPLATE_MODULE_NAME" 2>/dev/null)")"
+
+BOILERPLATE_DOM_MODULE_NAME="@mrpelz/boilerplate-dom"
+BOILERPLATE_DOM_MODULE_PATH="$(realpath --relative-to=. "$(npm ls --parseable --silent "$BOILERPLATE_DOM_MODULE_NAME" 2>/dev/null)")"
+
+SCRIPT_PATH="${BOILERPLATE_MODULE_PATH}/scripts"
+# shellcheck disable=SC1091
+source "${SCRIPT_PATH}/utils.sh"
+
+## FLOW
+
+# shellcheck disable=SC1091
+source "${SCRIPT_PATH}/common-pre.sh"
+
+if [[ $SKIP_FILES -ne 1 ]]; then
+	if check_response "🖇 install symbolic links referencing files in \"$BOILERPLATE_MODULE_NAME\" and \"$BOILERPLATE_DOM_MODULE_NAME\"?" y; then
+		make_ln "${BOILERPLATE_DOM_MODULE_PATH}/.editorconfig" .editorconfig
+
+		make_ln "${BOILERPLATE_DOM_MODULE_PATH}/.shellcheckrc" .shellcheckrc
+
+		make_ln "${BOILERPLATE_DOM_MODULE_PATH}/.vscode/extensions.json" .vscode/extensions.json
+		make_ln "${BOILERPLATE_DOM_MODULE_PATH}/.vscode/settings.json" .vscode/settings.json
+
+		make_ln "${BOILERPLATE_DOM_MODULE_PATH}/scripts/watch.sh" scripts/watch.sh
+		make_ln "${BOILERPLATE_DOM_MODULE_PATH}/scripts/watch-dev.sh" scripts/watch-dev.sh
+	fi
+
+	if check_response "📃 install bare config files extending base files in \"$BOILERPLATE_MODULE_NAME\" and \"$BOILERPLATE_DOM_MODULE_NAME\"?" y; then
+
+		# no indent
+		make_config .gitignore "$(
+			cat <<EOF
+.DS_Store
+dist
+node_modules
+secrets.txt
+EOF
+		)"
+
+		make_config Makefile "$(
+			cat <<EOF
+BASE_FILE := \$(shell npm ls --parseable --silent "$BOILERPLATE_DOM_MODULE_NAME" 2>/dev/null)
+
+include \$(BASE_FILE)/Makefile
+EOF
+		)"
+
+		make_config commitlint.config.mjs "$(
+			cat <<EOF
+// @ts-ignore
+import config from '$BOILERPLATE_DOM_MODULE_NAME/commitlint.config.mjs';
+
+/** @type {import('@commitlint/types').UserConfig} */
+export default config;
+EOF
+		)"
+
+		make_config eslint.config.js "$(
+			cat <<EOF
+// @ts-ignore
+import config from '$BOILERPLATE_DOM_MODULE_NAME/eslint.config.js';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default config;
+EOF
+		)"
+
+		make_config jest.config.js "$(
+			cat <<EOF
+// @ts-ignore
+import config from '$BOILERPLATE_DOM_MODULE_NAME/jest.config.js';
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default config;
+EOF
+		)"
+
+		make_config stylelint.config.js "$(
+			cat <<EOF
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    resolve(
+      dirname(fileURLToPath(import.meta.resolve('$BOILERPLATE_DOM_MODULE_NAME'))),
+      'stylelint.config.js',
+    ),
+  ],
+};
+EOF
+		)"
+
+		make_config webpack.config.js "$(
+			cat <<EOF
+// @ts-ignore
+import config from '$BOILERPLATE_DOM_MODULE_NAME/webpack.config.js';
+
+/** @type {import('webpack').Configuration | import('webpack').WebpackOptionsNormalized} */
+export default config;
+EOF
+		)"
+
+		make_config tsconfig.json "$(
+			cat <<EOF
+{
+  "compilerOptions": {
+    "outDir": "dist",
+  },
+  "extends": "$BOILERPLATE_DOM_MODULE_NAME/tsconfig.json",
+  "include": ["src/**/*"]
+}
+EOF
+		)"
+
+		make_config tsconfig.build.json "$(
+			cat <<EOF
+{
+  "compilerOptions": {
+    "noEmit": false
+  },
+  "exclude": ["**/*.test.ts"],
+  "extends": "./tsconfig.json",
+  "include": ["src/**/*"]
+}
+EOF
+		)"
+
+		make_config tsconfig.meta.json "$(
+			cat <<EOF
+{
+  "exclude": ["dist/**/*", "node_modules/**/*", "packages/**/*", "src/**/*"],
+  "extends": "$BOILERPLATE_DOM_MODULE_NAME/tsconfig.meta.json",
+  "include": ["**/*.js", "**/*.mjs"]
+}
+EOF
+		)"
+
+		make_config .gitlab-ci.yml "$(
+			cat <<EOF
+include:
+  - project: "mrpelz/boilerplate"
+    ref: main
+    file: "/gitlab/.gitlab-ci.yml"
+    # file: "/gitlab/.monorepo.gitlab-ci.yml"
+EOF
+		)"
+	fi
+fi
+
+# shellcheck disable=SC1091
+source "${SCRIPT_PATH}/common-post.sh"
+
+if check_response "📂 create \"static\" directory (this will *not* overwrite/empty out existing directories)?" y; then
+	mkdir -p static
+fi
+
+echo "✅ done"
--- packages/boilerplate-dom/scripts/watch-dev.sh
+++ packages/boilerplate-dom/scripts/watch-dev.sh
@@ -17,11 +17,11 @@ tmux \
 	set-option -p pane-border-format "eslint (eslint.config.json, includes files outside \"src\")" \; \
 	split-window -h -l 50% "make -s watch_test" \; \
 	set-option -p pane-border-format "jest (jest.config.json)" \; \
-	split-window -f -v -l 50% "make -s watch_build" \; \
-	set-option -p pane-border-format "tsc (tsconfig.build.json, excludes test-files)" \; \
+	split-window -f -v -l 50% "make -s watch_stylelint" \; \
+	set-option -p pane-border-format "stylelint (stylelint.config.js)" \; \
+	split-window -h -l 66% "make -s watch_build" \; \
+	set-option -p pane-border-format "webpack (ts-loader with tsconfig.build.json, excludes test-files)" \; \
 	split-window -h -l 50% "make -s watch_config" \; \
 	set-option -p pane-border-format "tsc (tsconfig.meta.json, checks files outside \"src\")" \; \
-	split-window -f -v -l 33% "make -s watch_run" \; \
-	set-option -p pane-border-format "node (\"dist/main.js\")" \; \
 	split-window -f -v -l 5 \; \
 	set-option -p pane-border-format "input" \; \
