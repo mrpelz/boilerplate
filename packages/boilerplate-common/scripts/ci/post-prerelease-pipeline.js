43ad8ec298d2004ecc26b29de6db831b70201225
Lennart Pelz <mail@mrpelz.de>
2025-01-08 17:13:07 +0100

    feat: post prerelease info

---
+++ packages/boilerplate-common/scripts/ci/post-prerelease-pipeline.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env -S node --experimental-modules
+/* eslint-disable no-console */
+
+import {
+  fetchMergeRequests,
+  fetchPipelineUser,
+  getEnvironment,
+  getFetchGitlab,
+  getProcessedBody,
+  getSlug,
+  replaceComments,
+} from './utils.js';
+
+(async () => {
+  try {
+    const environment = getEnvironment();
+    const fetchGitlab = getFetchGitlab(environment);
+
+    const {
+      CI_COMMIT_TAG: version,
+      CI_COMMIT_TAG_MESSAGE: tagMessage,
+      CI_PROJECT_URL: projectUrl,
+    } = process.env;
+    if (!projectUrl || !tagMessage || !version) return;
+
+    console.info({
+      CI_COMMIT_TAG: version,
+      CI_COMMIT_TAG_MESSAGE: tagMessage,
+      CI_PROJECT_URL: projectUrl,
+    });
+
+    const [body, headline] = getProcessedBody(`
+      ## Prerelease Pipeline
+
+      [Tag "${version}"](${projectUrl}/-/tags/${version}) has started [a release pipeline](${projectUrl}/-/pipelines/${environment.pipelineId}).
+    `);
+
+    const [projectMergeRequests, pipelineUsername] = await Promise.all([
+      fetchMergeRequests(fetchGitlab, environment.projectId),
+      fetchPipelineUser(
+        fetchGitlab,
+        environment.projectId,
+        environment.pipelineId,
+      ),
+    ]);
+
+    const matchingMergeRequests = projectMergeRequests.filter(
+      ({ source_branch }) => getSlug(source_branch) === tagMessage,
+    );
+
+    console.info(
+      `Found ${matchingMergeRequests.length} matching merge request(s).`,
+    );
+
+    await Promise.all(
+      matchingMergeRequests.map(async (matchingMergeRequest) =>
+        replaceComments(
+          fetchGitlab,
+          environment.projectId,
+          pipelineUsername,
+          matchingMergeRequest.iid,
+          headline,
+          body,
+        ),
+      ),
+    );
+  } catch (error) {
+    console.error(new Error('Error.', { cause: error }));
+    process.exit(1);
+  }
+})();
